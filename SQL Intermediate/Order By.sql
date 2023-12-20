367164b69e5ec2c346a36828353a30d567e22ad9
Emmanuel Itodo <136532526+Itozee@users.noreply.github.com>
2023-12-19 22:56:05 -0500

    Create Order By.sql

---
+++ SQL Intermediate/Order By.sql
@@ -0,0 +1,9 @@
+```
+-- SQL query to find the game with the highest sales in Japan and display its name and the sales number. 
+SELECT
+NAME
+,JP_Sales
+FROM VG_SALES
+ORDER BY JP_Sales DESC
+LIMIT 1;
+```
