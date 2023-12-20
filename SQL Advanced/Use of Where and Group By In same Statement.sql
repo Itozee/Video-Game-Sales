dba42faae7f75bff8f364639523c090da90e982b
Emmanuel Itodo <136532526+Itozee@users.noreply.github.com>
2023-12-20 13:20:55 -0500

    Create Use of Where and Group By In same Statement.sql

---
+++ SQL Advanced/Use of Where and Group By In same Statement.sql
@@ -0,0 +1,10 @@
+```
+-- Write a SQL query to find the publisher that has the highest number of games in the 'Sports' genre.
+
+SELECT
+Publisher
+,COUNT(Name) AS 'Highest Number Of Games'
+FROM vg_sales
+WHERE Genre = 'Sports'
+GROUP BY Publisher;
+```
